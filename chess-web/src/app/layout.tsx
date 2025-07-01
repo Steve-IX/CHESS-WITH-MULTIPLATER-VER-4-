f949e298d421f4c0b1513ef3101298478f4ef87f
Steve-IX <a.stephenyeboah04@gmail.com>
2025-07-01 18:32:43 +0100

    fix: Use relative path for import to fix Vercel build

--- chess-web/src/app/layout.tsx
+++ chess-web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { ThemeProvider } from '@/lib/ThemeContext';
+import { ThemeProvider } from '../lib/ThemeContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
